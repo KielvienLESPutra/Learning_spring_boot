558db5468c0d4316ed587f3f941ccff0d293b0a4
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-07 08:21:27 +0700

    first commit

---
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/LearningSpringBootApplication.java
@@ -0,0 +1,13 @@
+package my.id.kielvien.lesp.learning_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/id/kielvien/lesp/learning_spring_boot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package my.id.kielvien.lesp.learning_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
