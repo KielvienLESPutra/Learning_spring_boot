333c8b96a1376bce77bebaea04e41e5ad131fd93
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 05:40:04 +0700

    adding controller for user

---
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/KprClientController.java
@@ -0,0 +1,5 @@
+package my.id.kielvien.lesp.learning_spring_boot.api;
+
+public class KprClientController {
+
+}
---
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/UserController.java
@@ -0,0 +1,57 @@
+package my.id.kielvien.lesp.learning_spring_boot.api;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import my.id.kielvien.lesp.learning_spring_boot.models.UserModel;
+import my.id.kielvien.lesp.learning_spring_boot.service.UserService;
+
+@RequestMapping(value= "/user")
+@RestController
+public class UserController {
+	Logger logger = LoggerFactory.getLogger(UserController.class);
+	private UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping
+	public void postAddUser(@RequestBody UserModel user) {
+		logger.info("Start adding user to database with data : {}", user.toString());
+		userService.addUser(user);
+	}
+
+	@PutMapping(path = "{id}")
+	public void updateUser(@PathVariable("id") UUID id,@RequestBody UserModel user) {
+		userService.updateUser(id, user);
+	}
+	
+	@DeleteMapping(path = "{id}")
+	public void deleteUser(@PathVariable("id") UUID id) {
+		userService.deleteUser(id);
+	}
+
+	@GetMapping(path = "{id}")
+	public void searchUser(@PathVariable("id") UUID id) {
+		userService.searchUser(id);
+	}
+	
+	@GetMapping
+	public List<UserModel> getAllUser() {
+		return userService.getAllUser();
+	}
+}
