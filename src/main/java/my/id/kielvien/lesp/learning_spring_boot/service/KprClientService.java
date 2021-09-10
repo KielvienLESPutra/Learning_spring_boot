60a6ce325e4010212c11a35b3b89faa8e3f3155e
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 05:39:32 +0700

    adding service to call db layer

---
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/service/KprClientService.java
@@ -0,0 +1,5 @@
+package my.id.kielvien.lesp.learning_spring_boot.service;
+
+public class KprClientService {
+
+}
---
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/service/UserService.java
@@ -0,0 +1,47 @@
+package my.id.kielvien.lesp.learning_spring_boot.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import jdk.internal.org.jline.utils.Log;
+import my.id.kielvien.lesp.learning_spring_boot.dao.UserAccessService;
+import my.id.kielvien.lesp.learning_spring_boot.models.UserModel;
+
+@Service
+public class UserService {
+
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+	private UserAccessService userAccess;
+	
+	@Autowired
+	public UserService(@Qualifier("memory_database") UserAccessService userAccess) {
+		this.userAccess = userAccess;
+	}
+	
+	public void addUser(UserModel user) {
+		logger.info("Start adding user to database with data : {}", user.toString());
+		userAccess.addUser(user);
+	}
+	
+	public boolean updateUser(UUID id, UserModel user) {
+		return userAccess.updateUser(id, user);
+	}
+	
+	public boolean deleteUser(UUID id) {
+		return userAccess.deleteUser(id);
+	}
+	
+	public UserModel searchUser(UUID id) {
+		return userAccess.searchUser(id);
+	}
+	
+	public List<UserModel> getAllUser(){
+		return userAccess.showUser();
+	}
+}
