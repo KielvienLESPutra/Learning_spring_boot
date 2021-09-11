97836bd9a17d9fe360e9508d7ae1b54e3e09da6e
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 08:11:52 +0700

    adding process for all method

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/KprClientController.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/KprClientController.java
@@ -1,9 +1,19 @@
 package my.id.kielvien.lesp.learning_spring_boot.api;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import my.id.kielvien.lesp.learning_spring_boot.models.KprClientModel;
 import my.id.kielvien.lesp.learning_spring_boot.service.KprClientService;
 
 @RequestMapping(value= "/kpr")
@@ -14,5 +24,30 @@ public class KprClientController {
 	@Autowired
 	public KprClientController(KprClientService kprClientService) {
 		this.kprClientService = kprClientService;
+	}
+	
+	@PostMapping
+	public void postAddUser(@RequestBody KprClientModel kpr) {
+		kprClientService.addKprClient(kpr);
+	}
+
+	@PutMapping(path = "{id}")
+	public void updateUser(@PathVariable("id") UUID id,@RequestBody KprClientModel kpr) {
+		kprClientService.updateKprClient(id, kpr);
+	}
+	
+	@DeleteMapping(path = "{id}")
+	public void deleteUser(@PathVariable("id") UUID id) {
+		kprClientService.deleteKprClient(id);
+	}
+
+	@GetMapping(path = "{id}")
+	public KprClientModel searchUser(@PathVariable("id") UUID id) {
+		return kprClientService.searchKprClient(id);
+	}
+	
+	@GetMapping
+	public List<KprClientModel> getAllUser() {
+		return kprClientService.getAllKprClient();
 	}
 }
