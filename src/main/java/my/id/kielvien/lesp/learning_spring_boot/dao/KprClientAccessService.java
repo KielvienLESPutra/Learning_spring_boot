d8f4886bc5f8a15ba70776fd045406fa43e79c1a
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 07:08:00 +0700

    adding code for add kpr clint in list

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientAccessService.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientAccessService.java
@@ -1,18 +1,33 @@
 package my.id.kielvien.lesp.learning_spring_boot.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import my.id.kielvien.lesp.learning_spring_boot.models.KprClientModel;
-import my.id.kielvien.lesp.learning_spring_boot.models.UserModel;
 
 @Repository("memory_database_kpr")
 public class KprClientAccessService implements KprClientDao{
+	
+	Logger logger = LoggerFactory.getLogger(KprClientAccessService.class);
+	private List<KprClientModel> kprClients = new ArrayList<KprClientModel>();
+	
 	@Override
-	public boolean addKprClient(KprClientModel user) {
+	public boolean addKprClient(KprClientModel kpr) {
 		// TODO Auto-generated method stub
-		return false;
+		logger.info("Start adding kpr client to database with data : {}", kpr.toString());
+		boolean result = false;
+		try {
+			kprClients.add(kpr);
+			result = true;
+		}catch (Exception e) {
+			// TODO: handle exception
+			logger.error("Error exception adding user : " + kpr.toString(), e);
+		}
+		return result;
 	}
 
 	@Override
