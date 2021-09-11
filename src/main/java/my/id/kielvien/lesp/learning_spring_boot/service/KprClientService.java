09e030f13278309f7ba50a7e8d645d2201ae4dfc
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 07:46:49 +0700

    adding intial process kpr service to connect kpr service repository

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/service/KprClientService.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/service/KprClientService.java
@@ -1,5 +1,39 @@
 package my.id.kielvien.lesp.learning_spring_boot.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import my.id.kielvien.lesp.learning_spring_boot.dao.KprClientAccessService;
+import my.id.kielvien.lesp.learning_spring_boot.models.KprClientModel;
+
 public class KprClientService {
+	private KprClientAccessService kprAccess;
 	
+	@Autowired
+	public KprClientService(@Qualifier("memory_database_kpr") KprClientAccessService kprAccess) {
+		this.kprAccess = kprAccess;
+	}
+	
+	public void addUser(KprClientModel user) {
+		kprAccess.addKprClient(user);
+	}
+	
+	public boolean updateUser(UUID id, KprClientModel user) {
+		return kprAccess.updateKprClient(id, user);
+	}
+	
+	public boolean deleteUser(UUID id) {
+		return kprAccess.deleteKprClient(id);
+	}
+	
+	public KprClientModel searchUser(UUID id) {
+		return kprAccess.searchKprClient(id);
+	}
+	
+	public List<KprClientModel> getAllUser(){
+		return kprAccess.showKprClient();
+	}
 }
