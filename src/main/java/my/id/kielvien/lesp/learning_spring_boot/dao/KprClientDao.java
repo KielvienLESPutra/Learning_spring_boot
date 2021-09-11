00645f0440427a8372d016f524a91055a2003a33
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 07:19:01 +0700

    modify funtion search kpr by id

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientDao.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientDao.java
@@ -1,14 +1,14 @@
 package my.id.kielvien.lesp.learning_spring_boot.dao;
 
 import java.util.List;
+import java.util.UUID;
 
 import my.id.kielvien.lesp.learning_spring_boot.models.KprClientModel;
-import my.id.kielvien.lesp.learning_spring_boot.models.UserModel;
 
 public interface KprClientDao {
 	public boolean addKprClient(KprClientModel user);
 	public boolean updateKprClient();
 	public List<KprClientModel> showKprClient();
 	public boolean deleteKprClient();
-	public UserModel searchKprClient();
+	public KprClientModel searchKprClient(UUID id);
 }
