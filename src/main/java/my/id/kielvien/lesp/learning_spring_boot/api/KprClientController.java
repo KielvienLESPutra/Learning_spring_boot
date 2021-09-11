bc376df2f26171943818527e05d1ca1405804aec
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-09-11 08:07:31 +0700

    adding class constructor, auto injection, and path general rest controller

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/KprClientController.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/api/KprClientController.java
@@ -1,5 +1,18 @@
 package my.id.kielvien.lesp.learning_spring_boot.api;
 
-public class KprClientController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import my.id.kielvien.lesp.learning_spring_boot.service.KprClientService;
+
+@RequestMapping(value= "/kpr")
+@RestController
+public class KprClientController {
+	private KprClientService kprClientService;
+	
+	@Autowired
+	public KprClientController(KprClientService kprClientService) {
+		this.kprClientService = kprClientService;
+	}
 }
