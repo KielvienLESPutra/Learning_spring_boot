95e3cf959a181d226b7581b47e48a03228493f53
Kielvien Lourensiuse Eka Setia Putra <for2university@gmail.com>
2021-11-28 13:36:38 +0700

    adding annotation repository for access service

--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientDao.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/KprClientDao.java
@@ -3,8 +3,11 @@ package my.id.kielvien.lesp.learning_spring_boot.dao;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import my.id.kielvien.lesp.learning_spring_boot.models.KprClientModel;
 
+@Repository
 public interface KprClientDao {
 	public boolean addKprClient(KprClientModel user);
 	public List<KprClientModel> showKprClient();
--- src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/UserDao.java
+++ src/main/java/my/id/kielvien/lesp/learning_spring_boot/dao/UserDao.java
@@ -3,8 +3,11 @@ package my.id.kielvien.lesp.learning_spring_boot.dao;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import my.id.kielvien.lesp.learning_spring_boot.models.UserModel;
 
+@Repository
 public interface UserDao {
 	public boolean addUser(UserModel user);
 	public List<UserModel> showUser();
